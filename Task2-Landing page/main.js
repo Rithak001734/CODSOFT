d9d1a4a2d1957e0502b6ce6a1f024243b6b9a7ab
Rithak001734 <91362025+Rithak001734@users.noreply.github.com>
2023-10-07 01:58:26 +1300

    Add files via upload

---
+++ Task2-Landing page/main.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".anime-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+  
